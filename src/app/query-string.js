dc468dc6f9039dcaba4b5d53d043fb772f1bdac3
Cuzzo <yahn007@outlook.com>
2014-08-21 00:42:31 -0700

    Unit tests for QueryString::remove_character.

--- src/app/query-string.js
+++ src/app/query-string.js
@@ -8,12 +8,26 @@ var QueryString = function(update_callback, q) {
        _SPACE_REGEX = /\s+/g;
        _ESCAPED_SPACE_REGEX = /%20/g;
 
+  /**
+   * Remove a character from the querystring.
+   *
+   * @param int pos
+   *   The 0-index position of the character to remove.
+   */
   this.remove_character = function(pos) {
     if (pos > _q.length) return;
     var new_q = _q.substring(0, pos) + _q.substring(pos + 1);
     this.set(new_q);
   };
 
+  /**
+   * Inserts a character into the querystring.
+   *
+   * @param int pos
+   *   The 0-index position at which to insert a character.
+   * @param string char
+   *   The character to insert.
+   */
   this.insert_character = function(pos, char) {
     // Set pos to end if greater than length.
     pos = pos > _q.length ? _q.length : pos;
@@ -21,16 +35,34 @@ var QueryString = function(update_callback, q) {
     this.set(new_q);
   };
 
+  /**
+   * Sets the querystring to a string.
+   *
+   * @param string q
+   *   The string to set as the querystring.
+   */
   this.set = function(q) {
-    if (q === _q) return false;
+    if (typeof q !== "string" || q === _q) return;
     _q = q;
     update_callback(q);
   };
 
+  /**
+   * Gets the querystring.
+   *
+   * @return string
+   *   The querystring.
+   */
   this.get = function() {
     return _q;
   };
 
+  /**
+   * Escapes the querystring (to send as a URL parameter).
+   *
+   * @return string
+   *   The querystring, escaped--whitespace collapsed and replaced with "+".
+   */
   this.escape = function() {
     return encodeURIComponent(_q.replace(_SPACE_REGEX, " "))
         .replace(_ESCAPED_SPACE_REGEX, "+");
--- test/query-string.test.js
+++ test/query-string.test.js
@@ -53,4 +53,44 @@ describe("QueryString Unit Tests", function() {
       chai.assert.equal(q.set.callCount, 1);
     });
   });
+
+  describe("Remove Tests", function() {
+    var cb_stub;
+
+    beforeEach(function() {
+      cb_stub = sinon.stub();
+    });
+
+    it("Should remove from empty query string at 0.", function() {
+      var q = new QueryString(cb_stub, "");
+      q.remove_character(0);
+      chai.assert.equal(q.get(), "");
+    });
+
+    it("Should remove from beginning of string.", function() {
+      var q = new QueryString(cb_stub, "My Test String");
+      q.remove_character(0);
+      chai.assert.equal(q.get(), "y Test String");
+    });
+
+    it("Should remove from middle of string.", function() {
+      var q = new QueryString(cb_stub, "My Test String");
+      q.remove_character(1);
+      chai.assert.equal(q.get(), "M Test String");
+    });
+
+    it("Should remove from end of string.", function() {
+      var test_string = "My Test String";
+          q = new QueryString(cb_stub, test_string);
+      q.remove_character(test_string.length - 1);
+      chai.assert.equal(q.get(), "My Test Strin");
+    });
+
+    it("Should call set upon removal.", function() {
+      var q = new QueryString(cb_stub, "");
+      sinon.stub(q, "set", function(){});
+      q.remove_character(0, "");
+      chai.assert.equal(q.set.callCount, 1);
+    });
+  });
 });
